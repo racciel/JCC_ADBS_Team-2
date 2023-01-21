f082a7b0c1feeaa3bc9ea496758f7a4a567d4f4c
Andreas Leja <88081290+racciel@users.noreply.github.com>
2023-01-21 18:03:21 +0100

    UPDATE

--- connect.php
+++ connect.php
@@ -1,22 +1,17 @@
 <?php
 
-    $mysqli = new mysqli("rec.foi.hr", "leader", "pr0j3ctJ((", "project");
-    /*
-    @session_start();
-    $servername = "rec.foi.hr";
-    $username = "leader";
-    $password = "pr0j3ctJ((";
-    $dbname = "project";
-    */
-    
-    /* Attempt to connect to MySQL database */
-    /*
-    $link = mysqli_connect($servername, $username, $password, $dbname);
-    $conn = $link;
-    $con = $link;
-    */
-    // Check connection
-    /*if($link === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }*/
+    // HIHIHIIHIHIHIHIHIHIi
+    // 161.53.120.224
+    //$mysqli = new mysqli("rec.foi.hr:50432", "leader", "pr0j3ctJ((", "project");
+         $dbhost = 'rec.foi.hr:50432';
+         $dbuser = 'leader';
+         $dbpass = 'pr0j3ctJ((';
+         $mysqli = new mysqli($dbhost, $dbuser, $dbpass);
+         
+         if($mysqli→connect_errno ) {
+            printf("Connect failed: %s<br />", $mysqli→connect_error);
+            exit();
+         }
+         printf('Connected successfully.<br />');
+         $mysqli→close();
 ?>
