6a43d23426a78059832660a97570dfc5023664e0
BlackSnake <BlackSnake@DESKTOP-A5PG6J7>
2023-01-24 21:57:49 +0100

    Editen whole Title page 24.1/21:57

--- index.php
+++ index.php
@@ -1,12 +1,112 @@
 <?php 
     session_start();
     require('header.php');
+
 ?>
 
 <link rel="stylesheet" type="text/css" href="pocetna.css">
-Cupcake ipsum dolor sit amet gummi bears gummies brownie jelly. Gingerbread jelly gummi bears I love ice cream muffin fruitcake shortbread. Ice cream danish I love dessert marshmallow cotton candy pudding. Tiramisu donut toffee I love chocolate cake cotton candy I love pastry sweet roll. I love candy bear claw cheesecake chocolate jelly-o gummi bears cheesecake. Brownie I love bonbon gingerbread pastry jujubes. Gummi bears gingerbread cheesecake toffee bonbon soufflé. I love gingerbread croissant halvah carrot cake fruitcake powder chocolate cake. I love fruitcake macaroon chocolate cake jujubes sweet roll. I love chupa chups pudding chocolate bar chocolate. Lollipop gummies chocolate tart I love jujubes. Brownie lollipop bear claw I love jelly-o bear claw marshmallow chupa chups. Sweet muffin tiramisu marshmallow donut halvah brownie tart. Cotton candy I love brownie cheesecake pie halvah wafer I love lollipop.
-Chocolate cake I love marshmallow cake cookie soufflé pastry dessert marzipan. I love dragée pudding carrot cake bonbon candy canes caramels marshmallow croissant. Apple pie sweet chocolate bar tootsie roll sesame snaps toffee. Cotton candy icing donut muffin tootsie roll pudding marzipan. Ice cream I love tart marshmallow pie chocolate cake oat cake topping. Candy canes macaroon pastry danish ice cream marshmallow muffin. Dragée sesame snaps toffee biscuit sweet candy canes cake gingerbread. Muffin sweet bear claw chupa chups danish chocolate bar. Powder halvah gummies sweet apple pie dragée wafer. Chupa chups cake cheesecake cupcake caramels danish cheesecake sugar plum. Jelly muffin pudding donut marzipan chocolate I love sweet roll. Tart caramels gingerbread biscuit chocolate bar liquorice. I love topping I love gingerbread tart sweet I love cheesecake chupa chups.
-Pastry I love toffee dragée liquorice halvah dragée lemon drops. Jelly-o danish pudding I love biscuit tootsie roll candy canes cake gingerbread. Muffin halvah I love I love ice cream dessert bear claw bear claw carrot cake. Croissant marshmallow ice cream muffin cake I love biscuit cake chocolate cake. Toffee liquorice ice cream I love chocolate bar I love pastry dessert. Toffee danish carrot cake jelly beans ice cream chocolate cake I love I love. I love cookie jelly beans cookie icing jelly danish cupcake. Donut chocolate cake sesame snaps I love fruitcake cupcake apple pie. Sweet roll sweet shortbread I love tart gummies soufflé pudding jelly-o. Cake macaroon liquorice croissant I love carrot cake pudding. Sugar plum cookie jelly-o cookie bear claw brownie sugar plum dessert halvah. Marzipan cotton candy jujubes cupcake soufflé bonbon icing cookie. Jelly-o sweet wafer soufflé jelly-o pastry caramels cheesecake cookie.
-Chupa chups brownie sweet roll bear claw cotton candy chupa chups candy canes powder. Liquorice chocolate bar bear claw jelly-o sweet roll gummies chupa chups marshmallow cupcake. Sugar plum gummi bears lemon drops pie danish. Sweet roll oat cake bonbon I love jelly. Croissant brownie jelly-o pudding donut biscuit. Liquorice chocolate jelly-o croissant biscuit. Bear claw lollipop jelly candy canes chupa chups cookie. Croissant sweet roll oat cake shortbread pie. Icing cotton candy tiramisu lemon drops ice cream I love gummies sugar plum icing. Brownie dessert gummi bears brownie cheesecake dessert. Chocolate bar gummi bears I love fruitcake cheesecake wafer. Candy donut chocolate cake croissant macaroon sesame snaps cupcake. Cookie marshmallow I love pudding dessert.
-Pie candy cookie pastry chocolate bar chupa chups cookie danish brownie. Cake lemon drops chocolate chocolate cake I love. Dessert chocolate toffee I love lemon drops. Sesame snaps dessert jelly I love I love chocolate biscuit marzipan. Gummi bears brownie I love sweet icing gummies croissant caramels ice cream. Chocolate cake lollipop dessert liquorice cupcake sweet gingerbread chupa chups. I love topping I love jelly lollipop tart. Caramels chocolate cake brownie dessert macaroon I love. Bonbon I love fruitcake sesame snaps I love ice cream soufflé. Jelly beans cookie chocolate bar candy canes topping biscuit. Muffin cotton candy toffee icing donut I love jujubes. Candy canes cheesecake toffee brownie sweet roll I love.
+
+<div class="box">
+
+    <div class="box-content">
+
+
+        <div class="box-text">
+            <div class="wrap">
+                <?php
+                $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post ) as deso
+		where rowa = 1");
+
+                while ($row = pg_fetch_row($response)) {
+                echo "$row[2] \n" . "</br>";
+                }
+                ?>
+
+            </div>
+        </div>
+        <div class="box-footer">
+            <div class="wrap">
+                Lear More
+            </div>
+        </div>
+    </div>
+
+        <div class="box-content">
+            <div class="box-text">
+                <div class="wrap">
+                    <?php
+                    $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post ) as deso
+		where rowa = 2");
+
+                    while ($row = pg_fetch_row($response)) {
+                        echo "$row[2] \n" . "</br>";
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="box-footer">
+                <div class="wrap">
+                    Lear More
+                </div>
+            </div>
+        </div>
+
+</div>
+
+<div class="box">
+
+    <div class="box-content">
+
+
+        <div class="box-text">
+            <div class="wrap">
+                <?php
+                $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post ) as deso
+		where rowa = 3");
+
+                while ($row = pg_fetch_row($response)) {
+                    echo "$row[2] \n" . "</br>";
+                }
+                ?>
+
+            </div>
+        </div>
+        <div class="box-footer">
+            <div class="wrap">
+                Lear More
+            </div>
+        </div>
+    </div>
+
+    <div class="box-content">
+        <div class="box-text">
+            <div class="wrap">
+                <?php
+                $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post ) as deso
+		where rowa = 4");
+
+                while ($row = pg_fetch_row($response)) {
+                    echo "$row[2] \n" . "</br>";
+                }
+                ?>
+            </div>
+        </div>
+        <div class="box-footer">
+            <div class="wrap">
+                Lear More
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
 <?php require('footer.php');?>
