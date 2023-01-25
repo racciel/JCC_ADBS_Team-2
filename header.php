637bc5eee0922140fda7ff1bd26623dac41e3d7b
karlahlebec <92984453+karlahlebec@users.noreply.github.com>
2023-01-25 23:34:06 +0100

    Logs shown with the description of the code from table account_status

--- header.php
+++ header.php
@@ -60,7 +60,7 @@ Use this to check if your new user is added to the database
                 <li><a href="manageRoles.php">Manage roles</a></li>
                 <li><a href="managePermissions.php">Manage permissions</a></li>
                 <li><a href="usersList.php">Users list</a></li>
-                <li><a href="#">Logs</a></li>
+                <li><a href="logs.php">Logs</a></li>
                 <li><a href="#">Reports</a></li>
                     <?php } ?>
                 <li><a href="#">Some sort of info</a></li>
---
+++ logs.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+require('header.php');?>
+
+<div class="navigacija">
+    <div class="klasa navigation">
+        <div class="container manjeod">
+
+    </div>
+    </div>
+
+<div class="album py-4 bg-light pozadina">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <?php
+
+                $result = pg_query($conn, "Select l.user_name, l.date_time, l.code, a.description
+                 from account_status a inner join logs l on  l.code= a.code LIMIT 50;");
+
+                if($result) {
+                    echo('<table class="table josjedna">');
+                    echo('<tr><th scope="col"><label>Username</label></th><th scope="col"><label>Date and Time</label></th><th scope="col"><label>Code</label></th><th scope="col"><label>Description of code</label></th></tr>'); 
+                    while ($row = pg_fetch_row($result)) {
+                        echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
+                    }
+                    echo('</table>');
+                }
+                echo('<a href="index.php">Home page</a>')
+                ?>
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php require('footer.php');?>
