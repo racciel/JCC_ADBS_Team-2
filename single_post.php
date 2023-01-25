5dcb5a3cbc81405331850731cb22bd274a2e4674
BlackSnake <BlackSnake@DESKTOP-A5PG6J7>
2023-01-25 04:09:45 +0100

    Added opening window for Reporting while clicking on flag 70% code done 25.1./4:00

--- single_post.php
+++ single_post.php
@@ -91,7 +91,7 @@ while ($row = pg_fetch_row($response)) {
         <div class="box-content1" >
             <div class="box-text">
                 <div class="wrap">
-                    <p>Edit</p>
+                    <p class="edit">Edit</p>
                 </div>
             </div>
 
@@ -100,7 +100,7 @@ while ($row = pg_fetch_row($response)) {
         <div class="box-content2" >
             <div class="box-text" >
                 <div class="wrap" >
-                   <p class="report">Report <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 15 15" >
+                   <p class="report">Report <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 15 15"  cursor="pointer" onclick="openPopup()">
                            <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
                        </svg>
                    </p>
@@ -112,4 +112,23 @@ while ($row = pg_fetch_row($response)) {
 
     </div>
 
+    <div class="container">
+        <div class="popup" id="popup">
+            <h2>Test String</h2>
+            <p>test  text</p>
+            <button type="submit" onclick="closePopup()"> Send Report</button>
+        </div>
+    </div>
+<script>
+let popup = document.getElementById("popup");
+function openPopup()
+{
+popup.classList.add("open-popup")
+}
+function closePopup()
+{
+    popup.classList.remove("open-popup")
+}
+
+</script>
 <?php require('footer.php');?>
