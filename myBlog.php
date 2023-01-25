35a6da5320e39cfa97711fc7f1b161d6fe0086d4
BlackSnake <BlackSnake@DESKTOP-A5PG6J7>
2023-01-25 23:32:46 +0100

    Added opening window for Reporting while clicking on flag 70% code done 25.1./4:00

--- myBlog.php
+++ myBlog.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require('header.php');
-
+$var_userName =$_SESSION['username'];
 ?>
 <link rel="stylesheet" type="text/css" href="myBlogCSS.css">
 
@@ -18,7 +18,7 @@ require('header.php');
                 <?php
                 $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
 from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
-		from post ) as deso
+		from post where user_name ='$var_userName' ) as deso
 		where rowa = 1");
 
                 while ($row = pg_fetch_row($response)) {
@@ -43,8 +43,62 @@ from( select  post_id, Post_description, row_number() over(order by post_id desc
                 <?php
                 $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
 from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
-		from post ) as deso
+		from post where user_name ='$var_userName') as deso
 		where rowa = 2");
+
+                while ($row = pg_fetch_row($response)) {
+                    echo "$row[2] \n" . "</br>";
+                    $var_post_id2 = $row[1];
+                }
+                ?>
+            </div>
+        </div>
+        <div class="box-footer">
+            <div class="wrap" onclick='window.location.href ="single_post.php?var_post_id=<?php echo $var_post_id2 ?>"'>
+                Learn More
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="box">
+
+    <div class="box-content">
+
+
+        <div class="box-text">
+            <div class="wrap">
+                <?php
+                $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post where user_name ='$var_userName' ) as deso
+		where rowa = 3");
+
+                while ($row = pg_fetch_row($response)) {
+                    echo "$row[2] \n" . "</br>";
+                    $var_post_id1 = $row[1];
+                }
+                ?>
+
+            </div>
+        </div>
+        <div class="box-footer">
+            <div class="wrap" onclick='window.location.href ="single_post.php?var_post_id=<?php echo $var_post_id1 ?>"'>
+
+                Learn More
+            </div>
+        </div>
+    </div>
+
+    <div class="box-content">
+        <div class="box-text">
+            <div class="wrap">
+                <?php
+                $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post where user_name ='$var_userName') as deso
+		where rowa = 4");
 
                 while ($row = pg_fetch_row($response)) {
                     echo "$row[2] \n" . "</br>";
