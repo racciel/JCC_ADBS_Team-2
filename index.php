d89ae2e1f4b28dcf03ada2d544682a1c5a485926
BlackSnake <BlackSnake@DESKTOP-A5PG6J7>
2023-01-26 16:34:28 +0100

    Added opening window for Reporting while clicking on flag 70% code done 25.1./4:00

--- index.php
+++ index.php
@@ -116,5 +116,115 @@ from( select  post_id, Post_description, row_number() over(order by post_id desc
 
 </div>
 
+<div class="box">
+
+    <div class="box-content">
+
+
+        <div class="box-text">
+            <div class="wrap">
+                <?php
+                //$conn = pg_connect("host=rec.foi.hr port=50432 dbname=project user=leader password=pr0j3ctJ((");
+
+                $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from
+     ( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post ) as deso
+		where rowa = 5");
+
+                while ($row = pg_fetch_row($response)) {
+                    echo "$row[2] \n" . "</br>";
+                    $var_post_id1 = $row[1];
+                }
+                ?>
+
+            </div>
+        </div>
+        <div class="box-footer">
+            <div class="wrap" onclick='window.location.href ="single_post.php?var_post_id=<?php echo $var_post_id1 ?>"'>
+
+                Learn More
+            </div>
+        </div>
+    </div>
+
+    <div class="box-content">
+        <div class="box-text">
+            <div class="wrap">
+                <?php
+                $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post ) as deso
+		where rowa = 6");
+
+                while ($row = pg_fetch_row($response)) {
+                    echo "$row[2] \n" . "</br>";
+                    $var_post_id2 = $row[1];
+                }
+                ?>
+            </div>
+        </div>
+        <div class="box-footer">
+            <div class="wrap" onclick='window.location.href ="single_post.php?var_post_id=<?php echo $var_post_id2 ?>"'>
+                Learn More
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="box">
+
+    <div class="box-content">
+
+
+        <div class="box-text">
+            <div class="wrap">
+                <?php
+                $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post ) as deso
+		where rowa = 7");
+
+                while ($row = pg_fetch_row($response)) {
+                    echo "$row[2] \n" . "</br>";
+                    $var_post_id3 = $row[1];
+                }
+                ?>
+
+            </div>
+        </div>
+        <div class="box-footer">
+            <div class="wrap" onclick='window.location.href ="single_post.php?var_post_id=<?php echo $var_post_id3 ?>"'>
+                Learn More
+            </div>
+        </div>
+    </div>
+
+    <div class="box-content">
+        <div class="box-text">
+            <div class="wrap">
+                <?php
+                $response = pg_query($conn, "select rowa,post_id, substring(Post_description,1,700) 
+from( select  post_id, Post_description, row_number() over(order by post_id desc) as rowa
+		from post ) as deso
+		where rowa = 8");
+
+                while ($row = pg_fetch_row($response)) {
+                    echo "$row[2] \n" . "</br>";
+                    $var_post_id4 = $row[1];
+                }
+                ?>
+            </div>*
+        </div>
+        <div class="box-footer">
+            <div class="wrap" onclick='window.location.href ="single_post.php?var_post_id=<?php echo $var_post_id4 ?>"'>
+                Learn More
+            </div>
+        </div>
+    </div>
+
+</div>
+
 
 <?php require('footer.php');?>
