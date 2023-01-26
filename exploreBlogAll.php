35616c5e2a986552a14ca816660466e399f2b041
BlackSnake <BlackSnake@DESKTOP-A5PG6J7>
2023-01-26 22:13:28 +0100

    Some editing

---
+++ exploreBlogAll.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+require('header.php');
+
+?>
+<link rel="stylesheet" type="text/css" href="myBlogCSS.css">
+
+<br><h1>Welcome to all blogs </h1>
+
+<div class="box" style="padding: 1%">
+
+    <?php
+    // Sill need to implement datatable
+    $result = pg_query($conn, "select lifespan, post_title, substring(Post_description,1,700) || ' ...', Substr(cast(date_edited as text),3,10),post_id from post p;");
+
+    if($result) {
+        echo('<table id="table_id" class="table josjedna display" >');
+        echo('<thead><tr><th scope="col"><label>Post Title</label></th><th scope="col"><label>Post text</label></th><th scope="col"><label>Created at</label></th><th scope="col"><label>Show post</label></th></tr></thead>');
+        echo('<tbody>');
+        while ($row = pg_fetch_row($result)) {
+            $created = substr($row[0], 2, 10);
+            $var_post_id =$row[4];
+            echo "<tr><td>$row[1]</td><td>$row[2]</td><td>$created</td><td><a href='single_post.php?var_post_id=$var_post_id'>Show</a></td></tr>";
+        }
+        echo('</tbody>');
+        echo('</table>');
+    }
+
+    ?>
+
+</div>
+<?php echo('<a href="index.php">Home page</a>'); ?>
+<?php require('footer.php');?>
+
+
